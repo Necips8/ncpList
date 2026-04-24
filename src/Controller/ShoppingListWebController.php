900df2180d5fbaa432277ea83e2c9a57a2fed93b
necip <necips@live.de>
2026-04-24 17:50:24 +0200

    fix - security

--- src/Controller/ShoppingListWebController.php
+++ src/Controller/ShoppingListWebController.php
@@ -5,20 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class ShoppingListWebController extends AbstractController
 {
+    public function __construct(
+        private AuthorizationCheckerInterface $authChecker,
+    ) {}
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        // Falls nicht eingeloggt, zum Login leiten (später)
-        // Für den Prototyp zeigen wir die Hauptseite
+        if (!$this->authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('shopping_list/index.html.twig');
     }
 
     #[Route('/lists/{id}', name: 'app_list_detail')]
     public function detail(string $id): Response
     {
+        if (!$this->authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('shopping_list/detail.html.twig', [
             'listId' => $id,
         ]);
@@ -27,6 +39,11 @@ class ShoppingListWebController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
+        // Falls bereits eingeloggt, zur Startseite weiterleiten
+        if ($this->authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('shopping_list/login.html.twig');
     }
 }
